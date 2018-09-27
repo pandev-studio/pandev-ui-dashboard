2ba3d73e2738820ef1ee4a00ee5856ba9ec87a53
Umberto Peserico <umberto.peserico@pandev.it>
2018-09-27 10:10:02 +0000

    Resolve "Sistemazione della sidebar"

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,15 +1,58 @@
 $(document).ready(() => {
-    $('.sidebar').addClass(localStorage.getItem('sidebarOpened'));
+    const s = $('.sidebar');
+    s.addClass(localStorage.getItem('sidebarOpened'));
 
     $('.sidebar-toggler').click(() => {
-        const s = $('.sidebar');
-        if(s.hasClass('opened')) {
+        if (s.hasClass('opened')) {
             localStorage.setItem('sidebarOpened', '');
             $('.sidebar').removeClass('opened');
         } else {
             localStorage.setItem('sidebarOpened', 'opened');
             $('.sidebar').addClass('opened');
         }
-    })
+    });
+
+    $('.sidebar-dropdown-toggle').click(function () {
+        var sd = $(this).data('target');
+
+        if ($(sd).hasClass('open')) {
+            $(this).find('i.fas.fa-angle-down.right').remove();
+            $(this).append("<i class=\"fas fa-angle-up right\"></i>");
+
+            $(sd).removeClass('open');
+        } else {
+            $(this).find('i.fas.fa-angle-up.right').remove();
+            $(this).append("<i class=\"fas fa-angle-down right\"></i>");
+
+            $('.sidebar-dropdown-menu').removeClass('open');
+            $(sd).addClass('open');
+        }
+    });
+
+    $('.sidebar-actions-toggle').click(function () {
+        var sa = $(this).data('target');
+        var sam = $('.sidebar-actions-menu');
+
+        console.log(sa);
+
+        if (sam.data('status') === 'close') {
+            sam.animate({
+                'margin-left': 0
+            }, 400, function() {
+                sam.data('status', 'open');
+                console.log('Animazione completa apertura')
+            });
+            //sa.addClass('open-list');
+        } else {
+            sam.animate({
+                'margin-left': '-200px'
+            }, 400, function() {
+                sam.data('status', 'close');
+                console.log('Animazione completa chiusura')
+            });
+            //sa.removeClass('open-list');
+        }
+    });
+
 
 });
