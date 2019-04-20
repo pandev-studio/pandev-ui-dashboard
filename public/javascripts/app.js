4bb06536c99683266f7b61f115c1a41f73560812
Umberto Peserico <umberto.peserico@pandev.it>
2019-04-20 09:58:08 +0200

    11 - V2 - Stile pandev ui del drawer
    Completamento sidenav con i dropdown
    Dropdown nella navbar
    Breadcrumb navbar
    Cambio font size base a 14px

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,39 @@
-$('.sidenav-toggler').on('click', function () {
-    $('.drawer').toggleClass('sidenav-opened');
-    return false;
+$(function () {
+    $('.sidenav-toggler').on('click', function () {
+        $('.drawer').toggleClass('sidenav-opened');
+        $('.overlay').toggleClass('d-none');
+        return false;
+    });
+
+    $('.overlay').on('click', function () {
+       $('.sidenav-toggler').trigger('click');
+    });
+
+    $('.sidenav-dropdown-toggle').click(function () {
+        var sd = $(this).data('target');
+
+        if ($(sd).hasClass('open')) {
+            $(this).removeClass('opened');
+            $(this).find('.sidenav-link-caret').each(function (i, e) {
+                console.dir(e)
+                $(e).removeClass('fa-angle-down')
+                    .addClass('fa-angle-left');
+            });
+
+            $(sd).removeClass('open');
+        } else {
+            $(this).addClass('opened');
+            $(this).find('.sidenav-link-caret').each(function (i, e) {
+                $(e).removeClass('fa-angle-left')
+                    .addClass('fa-angle-down');
+            });
+
+            $('.sidenav-dropdown-menu').removeClass('open');
+            $(sd).addClass('open');
+        }
+
+        return false;
+    });
 });
+
+
